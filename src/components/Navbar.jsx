4a26634c2038894019d84be1906790ed614c3a59
iamakitten1 <m.kostamashvili.1@gmail.com>
2024-10-23 18:46:34 +0200

    flex

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,18 +9,18 @@ const Navbar = () => {
   };
 
   return (
-    <header className="flex justify-evenly items-center w-full h-full">
+    <header className="fixed top-0 left-0   w-full h-[80px] bg-white z-50 flex justify-evenly items-center ">
       <div className="text-[24px] text-[#2764D6] ">
         LINDEV
       </div>
       <div className="text-[20px] text-[#000000]">
         <nav>
           <ul className="flex items-center gap-[25px]">
-            <li  className="hover:font-light cursor-pointer uppercase" onClick={() => handleScroll("courses")}>კურსები</li>
-            <li className="hover:font-light cursor-pointer uppercase" onClick={() => handleScroll("aboutUs")}>ჩვენ შესახებ</li>
-            <li className="hover:font-light cursor-pointer uppercase"  onClick={() => handleScroll("blog")}>ბლოგი</li>
-            <li className="hover:font-light cursor-pointer uppercase" onClick={() => handleScroll("news")}>სიახლეები</li>
-            <li className="hover:font-light cursor-pointer uppercase" onClick={() => handleScroll("contact")}>კონტაქტი</li>
+            <li className=" hover:font-light cursor-pointer uppercase" onClick={() => handleScroll("courses")}>კურსები</li>
+            <li className="hover:font-light  uppercase" onClick={() => handleScroll("aboutUs")}>ჩვენ შესახებ</li>
+            <li className="hover:font-light uppercase"  onClick={() => handleScroll("blog")}>ბლოგი</li>
+            <li className="hover:font-light  uppercase" onClick={() => handleScroll("news")}>სიახლეები</li>
+            <li className="hover:font-light  uppercase" onClick={() => handleScroll("contact")}>კონტაქტი</li>
           </ul>
         </nav>
       </div>
